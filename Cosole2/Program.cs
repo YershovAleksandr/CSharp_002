124d308ebf7b16294b1074a54602553eaa180385
hp <namstudionsk@gmail.com>
2018-01-10 22:24:01 +0700

    Add all files

---
+++ Cosole2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cosole2
+{
+	class MainClass
+	{
+		private static int N = 10;
+
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+
+
+
+			for (int i = 0; i < N; i++)
+			{
+				Prn ();
+
+			}
+				
+		}
+
+		private static void Prn()
+		{
+			Console.Write ("#");
+		}
+	}
+}
